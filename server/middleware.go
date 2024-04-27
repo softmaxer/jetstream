72e75c1426c32a9c3b4cdf1bac7908254d4d7b47
Sriram Vadlamani <sriram.vadlamani@raccourci.fr>
2024-04-27 22:19:14 +0200

    Add active terminal log

--- server/middleware.go
+++ server/middleware.go
@@ -4,12 +4,17 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
 	"github.com/charmbracelet/wish/bubbletea"
 	"github.com/softmaxer/jetstream/views"
 )
 
 func TeaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
-	pty, _, _ := s.Pty()
+	pty, _, active := s.Pty()
+	if !active {
+		wish.Fatalln(s, "No active terminals! quitting.")
+		return nil, nil
+	}
 	renderer := bubbletea.MakeRenderer(s)
 	txtStyle := renderer.NewStyle().Foreground(lipgloss.Color("10"))
 	quitStyle := renderer.NewStyle().Foreground(lipgloss.Color("8"))
